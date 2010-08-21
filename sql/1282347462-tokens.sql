a229fe1ef84b8957620ba5d45a77322c345824f9
David E. Wheeler <david@justatheory.com>
2010-08-20 17:55:50 -0700

    Add interface for resettting passwords.
    * New table, `tokens`, keeps track of resetting tokens.
    * Call `forgot_password()` with a nickname and it returns a token and email
      address. Token expires after 1 day.
    * Call `reset_password()` with a token and new password to reset the pass.
      Updates the password only if the token hasn't expired.

---
+++ sql/1282347462-tokens.sql
@@ -0,0 +1,88 @@
+-- sql/1282347462-tokens.sql SQL Migration
+
+SET client_min_messages TO warning;
+SET log_min_messages    TO warning;
+
+BEGIN;
+
+CREATE TABLE tokens (
+    token      TEXT        PRIMARY KEY,
+    nickname   LABEL       NOT NULL REFERENCES users(nickname),
+    expires_at TIMESTAMPTZ NOT NULL DEFAULT NOW() + '1 day'::interval
+);
+
+CREATE OR REPLACE FUNCTION rand_str_of_len(
+    string_length INTEGER
+) RETURNS TEXT LANGUAGE 'plpgsql' STRICT AS $$
+DECLARE
+    chars TEXT = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    ret   TEXT = '';
+    pos   INTEGER;
+BEGIN
+    FOR i IN 1..string_length LOOP
+        pos := 1 + ( random() * ( length(chars) - 1) )::INTEGER;
+        ret := ret || substr(chars, pos, 1);
+    END LOOP;
+    RETURN ret;
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION forgot_password(
+    nick LABEL
+) RETURNS TEXT[] LANGUAGE plpgsql SECURITY DEFINER AS $$
+DECLARE
+    len  INTEGER := 5;
+    tok  TEXT;
+    nick LABEL;
+    mail EMAIL;
+BEGIN
+    SELECT nickname, email
+      INTO nick,     mail
+      FROM users
+     WHERE nickname = $1;
+
+    IF nick IS NULL THEN RETURN NULL; END IF;
+
+    LOOP BEGIN
+        tok := rand_str_of_len(len);
+        INSERT INTO tokens (token, nickname)
+        SELECT tok, nick;
+        IF FOUND THEN RETURN ARRAY[tok, mail]; END IF;
+    EXCEPTION WHEN unique_violation THEN
+        len := len + 1;
+        IF len >= 30 THEN
+            RAISE EXCEPTION '30-character id requested; something is wrong';
+        END IF;
+    END; END LOOP;
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION reset_password(
+    tok   TEXT,
+    pass  TEXT
+) RETURNS BOOLEAN LANGUAGE plpgsql SECURITY DEFINER AS $$
+DECLARE
+    nick LABEL;
+BEGIN
+    IF char_length(pass) < 4 THEN
+       RAISE EXCEPTION 'Password must be at least four characters long';
+    END IF;
+
+    DELETE FROM tokens
+     WHERE token = tok
+       AND expires_at >= NOW()
+    RETURNING nickname INTO nick;
+
+    IF nick IS NULL THEN RETURN FALSE; END IF;
+
+    UPDATE users
+       SET password = crypt(pass, gen_salt('des')),
+           updated_at = NOW()
+     WHERE nickname = nick;
+
+    RETURN TRUE;
+END;
+$$;
+
+
+COMMIT;
