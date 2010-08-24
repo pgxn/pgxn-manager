86e9963ce5e4493019b02805e0c88594e7ea3381
David E. Wheeler <david@justatheory.com>
2010-08-24 16:25:49 -0700

    Add `extensions` table.
    Also `coowners` and `distribution_extensions`.

---
+++ sql/1282690796-extensions.sql
@@ -0,0 +1,31 @@
+-- sql/1282690796-extensions.sql SQL Migration
+
+SET client_min_messages TO warning;
+SET log_min_messages    TO warning;
+
+BEGIN;
+
+CREATE TABLE extensions (
+    name  CITEXT           PRIMARY KEY,
+    owner LABEL            NOT NULL REFERENCES users(nickname),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE coowners (
+    extension  CITEXT      NOT NULL REFERENCES extensions(name),
+    nickname   LABEL       NOT NULL REFERENCES users(nickname),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (extension, nickname)
+);
+    
+CREATE TABLE distribution_extensions (
+    extension    CITEXT NOT NULL REFERENCES extensions(name),
+    ext_version  SEMVER NOT NULL,
+    distribution CITEXT NOT NULL,
+    dist_version SEMVER NOT NULL,
+    PRIMARY KEY (extension, ext_version, distribution, dist_version),
+    FOREIGN KEY (distribution, dist_version) REFERENCES distributions(name, version)
+);
+
+COMMIT;
