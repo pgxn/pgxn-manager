1f47af9c4cfe6726d3fff7b254131b904fa6c378
David E. Wheeler <david@justatheory.com>
2010-08-24 11:55:47 -0700

    Add `semver` data type.
    Implemented as a domain for now, but includes comparison operators. Might want
    to port it to C in the future. Shouldn't be too difficult.

--- sql/1282089310-types.sql
+++ sql/1282089310-types.sql
@@ -3,6 +3,8 @@
 SET client_min_messages TO warning;
 SET log_min_messages    TO warning;
 
+BEGIN;
+
 ------------------------------------------------------------------------------
 -- Create a timezone data type. This is really fast. See
 -- http://justatheory.com/computers/databases/postgresql/timezone_validation.html.
@@ -65,3 +67,192 @@ CREATE OR REPLACE FUNCTION is_uri(
 $$;
 
 CREATE DOMAIN uri AS CITEXT CHECK ( VALUE IS NULL OR is_uri( VALUE ) );
+
+------------------------------------------------------------------------------
+
+-- Create a semantic version data type.
+--
+-- http://semver.org/.
+--
+
+-- 1. A normal version number MUST take the form X.Y.Z where X, Y, and Z are
+-- integers. X is the major version, Y is the minor version, and Z is the
+-- patch version. Each element MUST increase numerically. For instance: 1.9.0
+-- < 1.10.0 < 1.11.0.
+--
+-- 2. A special version number MAY be denoted by appending an arbitrary string
+-- immediately following the patch version. The string MUST be comprised of
+-- only alphanumerics plus dash [0-9A-Za-z-] and MUST begin with an alpha
+-- character [A-Za-z]. Special versions satisfy but have a lower precedence
+-- than the associated normal version. Precedence SHOULD be determined by
+-- lexicographic ASCII sort order. For instance: 1.0.0beta1 < 1.0.0beta2 <
+-- 1.0.0.
+
+CREATE DOMAIN semver AS TEXT
+ CHECK ( VALUE ~ '^([1-9][0-9]*|0)[.]([1-9][0-9]*|0)[.]([1-9][0-9]*|0)([a-zA-Z][-0-9A-Za-z]*)?$' );
+
+CREATE FUNCTION semver_cmp(
+    lver semver,
+    rver semver
+) RETURNS INTEGER IMMUTABLE LANGUAGE plpgsql AS $$
+DECLARE
+    lparts TEXT[] := regexp_split_to_array(lver, '[.]');
+    rparts TEXT[] := regexp_split_to_array(rver, '[.]');
+    ret    INTEGER;
+    lstr   TEXT;
+    rstr   TEXT;
+BEGIN
+    ret := btint4cmp(lparts[1]::int, rparts[1]::int);
+    IF ret <> 0 THEN RETURN ret; END IF;
+
+    ret := btint4cmp(lparts[2]::int, rparts[2]::int);
+    IF ret <> 0 THEN RETURN ret; END IF;
+
+    lstr = substring(lparts[3] FROM '[a-zA-Z][-0-9A-Za-z]*$');
+    rstr = substring(rparts[3] FROM '[a-zA-Z][-0-9A-Za-z]*$');
+
+    IF lstr IS NULL THEN
+        IF rstr IS NULL THEN
+            -- No special string, just compare integers.
+            RETURN btint4cmp(lparts[3]::int, rparts[3]::int);
+        ELSE
+            ret := btint4cmp(lparts[3]::int, substring(rparts[3], '^[0-9]+')::int);
+            IF ret = 0 THEN
+                -- NULL is greater than non NULL.
+                RETURN 1;
+            ELSE
+                -- The numbers out-weigh the strings.
+                RETURN ret;
+            END IF;
+        END IF;
+    ELSE
+        IF rstr IS NULL THEN
+            ret := btint4cmp(substring(lparts[3], '^[0-9]+')::int, rparts[3]::int);
+            IF ret = 0 THEN
+                -- The string is less than the number.
+                RETURN -1;
+            ELSE
+                -- The numbers out-weigh the strings.
+                RETURN ret;
+            END IF;
+        ELSE
+            -- Both numbers have appended strings.
+            ret := btint4cmp(substring(lparts[3], '^[0-9]+')::int, substring(rparts[3], '^[0-9]+')::int);
+            IF ret = 0 THEN
+                -- Compare the strings case-insensitively.
+                RETURN bttext_pattern_cmp(LOWER(lstr), LOWER(rstr));
+            ELSE
+                -- The numeric comparision trumps the strings.
+                RETURN ret;
+            END IF;
+       END IF;
+    END IF;
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION semver_eq(
+    semver,
+    semver
+) RETURNS BOOLEAN LANGUAGE SQL AS $$
+    SELECT semver_cmp($1, $2) = 0;
+$$;
+
+CREATE OR REPLACE FUNCTION semver_ne(
+    semver,
+    semver
+) RETURNS BOOLEAN LANGUAGE SQL AS $$
+    SELECT semver_cmp($1, $2) <> 0;
+$$;
+
+CREATE OR REPLACE FUNCTION semver_lt(
+    semver,
+    semver
+) RETURNS BOOLEAN LANGUAGE SQL AS $$
+    SELECT semver_cmp($1, $2) < 0;
+$$;
+
+CREATE OR REPLACE FUNCTION semver_le(
+    semver,
+    semver
+) RETURNS BOOLEAN LANGUAGE SQL AS $$
+    SELECT semver_cmp($1, $2) <= 0;
+$$;
+
+CREATE OR REPLACE FUNCTION semver_gt(
+    semver,
+    semver
+) RETURNS BOOLEAN LANGUAGE SQL AS $$
+    SELECT semver_cmp($1, $2) > 0;
+$$;
+
+CREATE OR REPLACE FUNCTION semver_ge(
+    semver,
+    semver
+) RETURNS BOOLEAN LANGUAGE SQL AS $$
+    SELECT semver_cmp($1, $2) >= 0;
+$$;
+
+CREATE OPERATOR = (
+    LEFTARG    = SEMVER,
+    RIGHTARG   = SEMVER,
+    COMMUTATOR = =,
+    NEGATOR    = <>,
+    PROCEDURE  = semver_eq,
+    RESTRICT   = eqsel,
+    JOIN       = eqjoinsel,
+    HASHES,
+    MERGES
+);
+
+CREATE OPERATOR <> (
+    LEFTARG    = SEMVER,
+    RIGHTARG   = SEMVER,
+    NEGATOR    = =,
+    COMMUTATOR = <>,
+    PROCEDURE  = semver_ne,
+    RESTRICT   = neqsel,
+    JOIN       = neqjoinsel
+);
+
+CREATE OPERATOR < (
+    LEFTARG    = SEMVER,
+    RIGHTARG   = SEMVER,
+    NEGATOR    = >=,
+    COMMUTATOR = >,
+    PROCEDURE  = semver_lt,
+    RESTRICT   = scalarltsel,
+    JOIN       = scalarltjoinsel
+);
+
+CREATE OPERATOR <= (
+    LEFTARG    = SEMVER,
+    RIGHTARG   = SEMVER,
+    NEGATOR    = >,
+    COMMUTATOR = >=,
+    PROCEDURE  = semver_le,
+    RESTRICT   = scalarltsel,
+    JOIN       = scalarltjoinsel
+);
+
+CREATE OPERATOR >= (
+    LEFTARG    = SEMVER,
+    RIGHTARG   = SEMVER,
+    NEGATOR    = <,
+    COMMUTATOR = <=,
+    PROCEDURE  = semver_ge,
+    RESTRICT   = scalargtsel,
+    JOIN       = scalargtjoinsel
+);
+
+CREATE OPERATOR > (
+    LEFTARG    = SEMVER,
+    RIGHTARG   = SEMVER,
+    NEGATOR    = <=,
+    COMMUTATOR = <,
+    PROCEDURE  = semver_gt,
+    RESTRICT   = scalargtsel,
+    JOIN       = scalargtjoinsel
+);
+
+
+COMMIT;
