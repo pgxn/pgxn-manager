7d2e36134e780aae6fb59ae8aafa38062d1fa2e3
David E. Wheeler <david@justatheory.com>
2010-08-28 19:47:18 -0700

    Implement and test `add_coowner()`.

--- sql/1283039854-ownership.sql
+++ sql/1283039854-ownership.sql
@@ -1,8 +1,8 @@
 -- sql/1283039854-ownership.sql SQL Migration
 
 CREATE OR REPLACE FUNCTION is_admin_or_owns(
-    nick          LABEL,
-    VARIADIC exts TEXT[]
+    nick LABEL,
+    exts TEXT[]
 ) RETURNS BOOLEAN LANGUAGE plpgsql STABLE AS $$
 DECLARE
     ret BOOLEAN;
@@ -23,20 +23,46 @@ $$;
 -- Disallow end-user from using this function.
 REVOKE ALL ON FUNCTION is_admin_or_owns(LABEL, TEXT[]) FROM PUBLIC;
 
-CREATE OR REPLACE FUNCTION insert_coowner(
-    owner         LABEL,
-    coowner       LABEL,
-    VARIADIC exts TEXT[]
+CREATE OR REPLACE FUNCTION add_coowner(
+    nick    LABEL,
+    coowner LABEL,
+    exts    TEXT[]
 ) RETURNS BOOLEAN LANGUAGE PLPGSQL SECURITY DEFINER AS $$
+/*
+
+    SELECT insert_cowoner('theory', 'strongrrl', ARRAY['pair', 'pgtap']);
+
+Grants co-ownership to one or more extensions. The first argument is the
+nickname of the uesr inserting the co-owner. Said user must either be and
+admin or own *all* of the specified extensions. The second argument is the
+nickname of the user being granted co-ownership. This name must not be the
+same name as the owner. The third argument is an array of the names of the
+extensions to which co-ownership is to be granted.
+
+*/
 BEGIN
--- Can be done by admin or owner.
+    IF NOT is_admin_or_owns(nick, exts) THEN
+        RAISE EXCEPTION 'User “%” does not have permission to grant co-ownership to “%”',
+            nick, array_to_string(exts, '”, “');
+    END IF;
+
+    -- Grant only if the target is not already owner or co-owner.
+    INSERT INTO coowners (extension, nickname)
+    SELECT e.name, coowner
+      FROM extensions e
+      LEFT JOIN coowners c ON e.name = c.extension AND c.nickname = coowner
+     WHERE e.name     = ANY(exts)
+       AND e.owner    <> coowner
+       AND c.nickname IS NULL;
+
+    RETURN FOUND;
 END;
 $$;
 
-CREATE OR REPLACE FUNCTION delete_coowner(
-    owner         LABEL,
-    coowner       LABEL,
-    VARIADIC exts TEXT[]
+CREATE OR REPLACE FUNCTION remove_coowner(
+    owner   LABEL,
+    coowner LABEL,
+    exts    TEXT[]
 ) RETURNS BOOLEAN LANGUAGE PLPGSQL SECURITY DEFINER AS $$
 BEGIN
 -- Can be done by admin, owner, or coowner (to self)
@@ -44,9 +70,9 @@ END;
 $$;
 
 CREATE OR REPLACE FUNCTION transfer_ownership(
-    owner         LABEL,
-    coowner       LABEL,
-    VARIADIC exts TEXT[]
+    owner   LABEL,
+    coowner LABEL,
+    exts    TEXT[]
 ) RETURNS BOOLEAN LANGUAGE PLPGSQL SECURITY DEFINER AS $$
 BEGIN
 -- Can be done by admin or owner.
